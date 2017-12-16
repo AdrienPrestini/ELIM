e00c55413579257867be65915f0deb66d375256a
Adrien Prestini <adrienprestini@hotmail.fr>
2017-12-16 23:14:59 +0100

    ajout de la position GPS dans un service et correction des bugs du GPS

--- Dev/app/src/main/java/com/pepin_prestini/elim/elim/MainActivity.java
+++ Dev/app/src/main/java/com/pepin_prestini/elim/elim/MainActivity.java
@@ -1,20 +1,59 @@
 package com.pepin_prestini.elim.elim;
 
+import android.Manifest;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.pepin_prestini.elim.elim.Services.GPSService;
+
 
 public class MainActivity extends AppCompatActivity {
 
+
+    TextView mLatitudeTextView ;
+    TextView mLongitudeTextView ;
+    private boolean serviceIsStarted = false;
+    private IntentFilter intentFilter;
+    FloatingActionButton fab;
+    private boolean GPSEnable = false;
+
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Double lat = intent.getDoubleExtra(GPSService.LAT,0);
+            Double lon = intent.getDoubleExtra(GPSService.LONG,0);
+            mLatitudeTextView.setText(lat.toString());
+            mLongitudeTextView.setText(lon.toString());
+        }
+    };
+
+        public static final int MY_PERMISSIONS_REQUEST_GPS = 123;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.AppTheme);
+        setTheme(R.style.AppTheme_AppBarOverlay);
+
         super.onCreate(savedInstanceState);
 
         try {
@@ -27,15 +66,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        mLatitudeTextView =  findViewById((R.id.latitude_textview));
+        mLongitudeTextView = findViewById((R.id.longitude_textview));
+        fab = findViewById(R.id.fab);
+        fab.setBackgroundTintList(ColorStateList.valueOf(Color
+                .parseColor("#C50D0C")));
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+                startGPSPreview();
             }
         });
+
+
+        intentFilter = new IntentFilter();
+        intentFilter.addAction(GPSService.SERVICE_TO_ACTIVITY);
+        registerReceiver(receiver, intentFilter);
     }
 
     @Override
@@ -59,5 +107,139 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void requestLocationPermission() {
+        // Permission has not been granted and must be requested.
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Provide an additional rationale to the user if the permission was not granted
+            // and the user would benefit from additional context for the use of the permission.
+            // Display a SnackBar with a button to request the missing permission.
+            Snackbar.make(findViewById(R.id.main_layout), "Le GPS est necessaire pour accèder à la position",
+                    Snackbar.LENGTH_INDEFINITE).setAction("OK", new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    // Request the permission
+                    ActivityCompat.requestPermissions(MainActivity.this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            MY_PERMISSIONS_REQUEST_GPS);
+                }
+            }).show();
+
+        } else {
+            Snackbar.make(findViewById(R.id.main_layout),
+                    "Permission non disponible. Demande de permission pour le GPS",
+                    Snackbar.LENGTH_SHORT).show();
+            // Request the permission. The result will be received in onRequestPermissionResult().
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_GPS);
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                           int[] grantResults) {
+        // BEGIN_INCLUDE(onRequestPermissionsResult)
+        if (requestCode == MY_PERMISSIONS_REQUEST_GPS) {
+            // Request for camera permission.
+            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission has been granted. GPS camera preview Activity.
+                /*Snackbar.make(findViewById(R.id.main_layout), "GPS autorisé. Démarrage du GPS.",
+                        Snackbar.LENGTH_SHORT)
+                        .show();*/
+                startGPS();
+            } else {
+                // Permission request was denied.
+                /*Snackbar.make(findViewById(R.id.main_layout), "Le GPS n'est pas autorisé.",
+                        Snackbar.LENGTH_SHORT)
+                        .show();*/
+            }
+        }
+        // END_INCLUDE(onRequestPermissionsResult)
+    }
+
+    private void startGPS() {
+        verifyGPS();
+        if(GPSEnable) {
+            if (!serviceIsStarted) {
+                fab.setBackgroundTintList(ColorStateList.valueOf(Color
+                        .parseColor("#40900e")));
+                startService(new Intent(this, GPSService.class));
+                Toast.makeText(this.getApplicationContext(), "ON COMMENCE LE SERVICE", Toast.LENGTH_LONG).show();
+            } else {
+                fab.setBackgroundTintList(ColorStateList.valueOf(Color
+                        .parseColor("#C50D0C")));
+                Toast.makeText(this.getApplicationContext(), "ON ARRETE LE SERVICE", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent();
+                intent.setAction(GPSService.ACTIVITY_TO_SERVICE);
+
+                intent.putExtra(GPSService.STOP, 0);
+                sendBroadcast(intent);
+                //stopService(new Intent(this, GPSService.class));
+            }
+            serviceIsStarted = !serviceIsStarted;
+        }
+
+
+    }
+
+    private void startGPSPreview() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            // Permission is already available, start camera preview
+            /*Snackbar.make(findViewById(R.id.main_layout),
+                    "La permission de localisation GPS  is available. Starting preview.",
+                    Snackbar.LENGTH_SHORT).show();*/
+            startGPS();
+        } else {
+            // Permission is missing and must be requested.
+            requestLocationPermission();
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(receiver, intentFilter);
+        verifyGPS();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    private void verifyGPS(){
+        final LocationManager manager = (LocationManager) getSystemService( Context.LOCATION_SERVICE );
+        GPSEnable = manager.isProviderEnabled( LocationManager.GPS_PROVIDER );
+        if (!manager.isProviderEnabled( LocationManager.GPS_PROVIDER ) || !manager.isProviderEnabled( LocationManager.NETWORK_PROVIDER ) ) {
+
+            // Build the alert dialog
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Location Services Not Active");
+            builder.setMessage("Please enable Location Services and GPS");
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    // Show location settings when the user acknowledges the alert dialog
+                    Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                    startActivity(intent);
+                }
+            });
+            Dialog alertDialog = builder.create();
+            alertDialog.setCanceledOnTouchOutside(false);
+            alertDialog.show();
+        }
+
     }
 }
--- Dev/app/src/main/java/com/pepin_prestini/elim/elim/PreferencesActivity.java
+++ Dev/app/src/main/java/com/pepin_prestini/elim/elim/PreferencesActivity.java
@@ -1,15 +1,42 @@
 package com.pepin_prestini.elim.elim;
 
+import android.content.SharedPreferences;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
-public class PreferencesActivity extends AppCompatActivity {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class PreferencesActivity extends PreferenceActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_preferences);
-        Toast.makeText(getApplicationContext(), "Vous êtes dans les préférences", Toast.LENGTH_LONG).show();
+        //addPreferencesFromResource(R.xml.preference);
+        //Toast.makeText(getApplicationContext(), "Vous êtes dans les préférences", Toast.LENGTH_LONG).show();
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new MyPreferenceFragment()).commit();
+
+        getValuesPreference();
+    }
+
+    private void getValuesPreference(){
+        SharedPreferences SP = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+        boolean checkBoxNotification = SP.getBoolean("notificationStores",true);
+        Set<String> choicesStoreTypes = SP.getStringSet("StoreTypes", null);
+    }
+    public static class MyPreferenceFragment extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(final Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preference);
+        }
     }
 }
